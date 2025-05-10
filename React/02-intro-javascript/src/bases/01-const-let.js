c91894074f8172da37c718b16749222b6996e591
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-10 07:39:16 -0500

    VariablesYConstantes

---
+++ React/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+// Variables y Constantes
+
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado);
+
+// Var no se debe usar
+if (true) {
+    const nombre = 'Peter';
+
+    let valorDado = 6;
+
+    console.log(valorDado, nombre);
+    
+}
+
+console.log( valorDado );
