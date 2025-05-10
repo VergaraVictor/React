7249f49881b0da0c3396e33a467f64cec48770a0
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-10 07:48:17 -0500

    TemplateString

---
+++ React/02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,16 @@
+
+
+
+const nombre = 'Victor';
+const apellido = 'Vergara';
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+console.log( nombreCompleto );
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo( nombre ) } ` );
