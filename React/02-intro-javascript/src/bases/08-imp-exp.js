80245f997533bf822a89dcb3657deecd74757542
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-13 08:10:21 -0500

    MultiplesExportacionesYExportacionesPorDefecto

---
+++ React/02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,29 @@
+// import { heroes } from './data/heroes.js';
+// import { heroes } from './data/heroes';
+import heroes, { owners } from '../data/heroes';
+
+console.log( owners );
+
+// const getHeroeById = (id) => {
+//     return heroes.find(( heroe ) => {
+//         if (heroe.id === id ) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });
+// }
+
+// ( heroe ) este es el argumento de la funcion
+
+const getHeroeById = (id) => {
+    return heroes.find( ( heroe ) => heroe.id === id );
+}
+
+console.log( getHeroeById(2) );
+
+// find?, filter
+
+const getHeroeByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner );
+
+console.log( getHeroeByOwner('Marvel') );
