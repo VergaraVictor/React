26cc7dc27127fa6373d9797825e518d1400c58d8
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-19 16:10:19 -0500

    ImpresionDeVariablesEnElHTMl

--- React/03-counter-app-vite/src/FirstApp.jsx
+++ React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,25 @@
+// function MensajeVergara () {
+//     return (<h1>Todo es Posible</h1>);
+// }
+
+// const newMessage = {
+//     message: 'Hola Mundo',
+//     title: 'Fernando'
+// };
+
+// Si no hay alguna dependencia colocar las funciones u objetos fuera
+const getResult = (a, b) => {
+    return a + b;
+}
+
+
 export const FirstApp = () => {
+
     return (
-        <>
-            <h1> Victor Vergara</h1>
+        <>  
+            {/* <h1>{'MensajeVergara'}</h1> */}
+            {/* <code>{ JSON.stringify( newMessage ) }</code> */}
+            <h1>{ getResult(1,2) }</h1>
             <p>Soy un subtitulo</p>
         </>
     );
