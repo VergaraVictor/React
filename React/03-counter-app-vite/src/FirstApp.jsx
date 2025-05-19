5fd0dc6a675c71c155030b45dff1afd936cf52ed
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-19 16:30:15 -0500

    ComunicacionEntreComponentes-Props

--- React/03-counter-app-vite/src/FirstApp.jsx
+++ React/03-counter-app-vite/src/FirstApp.jsx
@@ -8,19 +8,22 @@
 // };
 
 // Si no hay alguna dependencia colocar las funciones u objetos fuera
-const getResult = (a, b) => {
-    return a + b;
-}
+// const getResult = (a, b) => {
+//     return a + b;
+// }
 
 
-export const FirstApp = () => {
-    
+export const FirstApp = ({ title, subTitle }) => {
+
+    // console.log(title);
+
     return (
         <>  
             {/* <h1>{'MensajeVergara'}</h1> */}
             {/* <code>{ JSON.stringify( newMessage ) }</code> */}
-            <h1>{ getResult(1,2) }</h1>
-            <p>Soy un subtitulo</p>
+            {/* <h1>{ getResult(1,2) }</h1> */}
+            <h1>{ title }</h1>
+            <p>{ subTitle + 1 }</p>
         </>
     );
 }
