d83d3c1b0b6d39071d92b25ee6074819e53c5027
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-20 12:12:42 -0500

    TareaComponenteCounterApp

--- React/03-counter-app-vite/src/main.jsx
+++ React/03-counter-app-vite/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from "./FirstApp";
+// import { FirstApp } from "./FirstApp";
 import './styles.css'
+import { CounterApp } from "./CounterApp";
 
 
 ReactDOM.createRoot(document.getElementById('root') ).render(
     <React.StrictMode>
-        {/* <HelloWorldApp /> */}
-        <FirstApp />
+        <CounterApp value={ 10 } />
     </React.StrictMode>
 )
