a0ef3291b5073a612845d87ab4d49ea0bb3e9382
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-21 07:58:34 -0500

    handleSubstractYHandleReset

--- React/03-counter-app-vite/src/CounterApp.jsx
+++ React/03-counter-app-vite/src/CounterApp.jsx
@@ -5,6 +5,9 @@ export const CounterApp = ({ value }) => {
 
   const [ counter, setCounter ] = useState( value );
 
+    console.log('render');
+    // API... FETCH... ... Tener presente que cuando hay un cambio en el estado va actualizilar toda la funcion o componente. 
+
   // function handleAdd(event, newValue ) { 
   //   console.log(newValue) 
   // }
@@ -14,14 +17,24 @@ export const CounterApp = ({ value }) => {
     // setCounter( (c) => c + 1) Otra manera de hacerlo
   }
 
+  // const handleSubstract = () => {
+  //   setCounter( counter - 1);
+  // } otra forma que es quitar la funcion de flecha
+  const handleSubstract = () => setCounter( counter - 1);
+
+  // const handleReset = () => {
+  //   setCounter( value );
+  // }
+  const handleReset = () => setCounter( value );
+
   return (
     <>
         <h1>CounterApp</h1>
         <h2> { counter } </h2>
 
-        <button onClick={ handleAdd }>
-          +1
-        </button>
+        <button onClick={ handleAdd }> +1 </button>
+        <button onClick={ handleSubstract }> -1 </button>
+        <button onClick={ handleReset }> Reset </button>
     </>
   );
 }
