11049baf41fe38545134ded5588f4ecb4ac66012
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-21 07:25:35 -0500

    EventoClickEventosEnGeneral

--- React/03-counter-app-vite/src/CounterApp.jsx
+++ React/03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,25 @@
 import PropTypes from "prop-types"
 
 export const CounterApp = ({ value }) => {
+
+  // function handleAdd(event, newValue ) { 
+  //   console.log(newValue) 
+  // }
+  const handleAdd = () => { 
+    console.log('+1');
+    value = 1000; 
+    console.log(value);
+    
+  }
+
   return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value }</h2>
+        <h2> { value } </h2>
+
+        <button onClick={ handleAdd }>
+          +1
+        </button>
     </>
   );
 }
