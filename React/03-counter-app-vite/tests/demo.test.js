c88489fc6c8bb68d110c3f030cf1058fea27a6b8
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-22 08:00:54 -0500

    JestExpectToBe

--- React/03-counter-app-vite/tests/demo.test.js
+++ React/03-counter-app-vite/tests/demo.test.js
@@ -1,6 +1,14 @@
-test('Esta prueba no debe fallar', () => {
+describe('Pruebas en <DemoComponent />', () => {
+    
+    test('Esta prueba no debe fallar', () => {
 
-    if ( 0 === 1 ) {
-        throw new Error('No puede dividir entre 0');
-    }
-})
+        // 1. Inicialización
+        const message1 = 'Hola Mundo';
+
+        // 2. Estímulo
+        const message2 = message1.trim();
+        
+        // 2. Observar el comportamiento... esperado
+        expect( message1 ).toBe( message2 );
+    })
+});
