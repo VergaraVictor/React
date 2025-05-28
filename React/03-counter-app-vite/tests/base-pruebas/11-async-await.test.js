e2e596d348d3e1db56134c7243fc729d0c08bac1
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-05-28 07:45:19 -0500

    EvaluarElCatchEnElAsyncAwait

--- React/03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
+++ React/03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -2,11 +2,10 @@ import { getImagen } from "../../src/base-pruebas/11-async-await";
 
 describe('Pruebas en 11-async-await', () => {
 
-    test('getImangen debe retornar un URL de la imagen', async() => {
-        const url = await getImagen();
-        console.log(url);  
+    test('getImangen debe retornar un URL de la imagen debe retornar un error si no tenemos api key', async() => {
+        const resp = await getImagen();  
         
-        expect ( typeof url ).toBe('string');
+        expect ( resp ).toBe('No se encontro la imagen');
     });
     
 })
