626b0b8b06f0a08ed32bd842e4cd3a092f035846
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-03 07:47:37 -0500

    GifExpertAppComponent

--- React/04-gif-expert-app/src/GifExpertApp.jsx
+++ React/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,14 @@
 
 export const GifExpertApp = () => {
   return (
-    <>
+    <>  
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+            {/* Git Item */}
     </>
 
   )
