04cd3774792f53dec5671bb05404ae4c84f2b8ac
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-03 07:39:02 -0500

    InicioDeProyectoGitExpertApp

---
+++ React/04-gif-expert-app/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GifExpertApp />
+  </StrictMode>
+);
+
+
+// Api TfioeJvgjs0Kit3RlqVynZBDjQMSdo8o
