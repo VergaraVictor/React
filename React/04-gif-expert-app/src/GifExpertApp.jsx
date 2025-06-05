2ca31257039585e574cabd327272d3f1d53d72d7
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-05 08:06:12 -0500

    GifGridNuevoComponente

--- React/04-gif-expert-app/src/GifExpertApp.jsx
+++ React/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
@@ -29,14 +30,16 @@ export const GifExpertApp = () => {
 
             {/* Listado de Gif */}
             {/* <button onClick={ onAddCategory }>Agregar</button> */}
-            <ol>
-                { categories.map( category => {
-                        return <li key={ category }>{ category }</li>
-                    }) 
-                }
-                {/* <li>ABC</li> */}
-            </ol>
-                {/* Git Item */}
+        
+            { 
+                categories.map( (category) => (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category } />
+                )) 
+            }
+            
+            {/* Git Item */}
         </>
 
     )
