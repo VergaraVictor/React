6c671ab34a52934304a19bcc336b2cbdea90e44d
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-07 08:10:15 -0500

    DemostracionDeProduccionRapido

--- React/04-gif-expert-app/src/components/GifGrid.jsx
+++ React/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-
-    const [counter, setCounter] = useState(10)
 
     useEffect( () => {
         getGifs(category);
@@ -12,9 +10,6 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{ category }</h3>
-            
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter( counter + 1)} >+1</button>
 
         </>
     )
