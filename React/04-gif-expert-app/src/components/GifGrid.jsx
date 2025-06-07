51a92a2a15cec383ecba0c2227a9526ed9849cc5
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-07 07:32:03 -0500

    FetchAPIObtenerLasImagenesDeseadas

--- React/04-gif-expert-app/src/components/GifGrid.jsx
+++ React/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,14 @@
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
     
-    // const gifs = [1,2,3,4,5]
-    
+    getGifs(category);
+
     return (
         <>
             <h3>{ category }</h3>
             {
-                // gifs.map( gif => (
-                //     <p>{ gif }</p>
-                // ))
+                
             }
         </>
     )
