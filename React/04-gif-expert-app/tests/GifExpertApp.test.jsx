6f4b2c011a860f99544a0df1fdd08c0acb78f6bd
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-21 08:12:37 -0500

    PruebasDeTarea

---
+++ React/04-gif-expert-app/tests/GifExpertApp.test.jsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import { GifExpertApp } from "../src/GifExpertApp";
+
+describe('Puebas en <GifExpertApp />', () => {
+    test('should ', () => {
+        
+        render( <GifExpertApp /> )
+        screen.debug();
+
+    })
+    
+})
