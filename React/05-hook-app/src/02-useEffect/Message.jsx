cff9a92e5074f7c123b6341f8dbe2754aeecb88c
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-27 07:57:55 -0500

    useEffectPrecauciones

--- React/05-hook-app/src/02-useEffect/Message.jsx
+++ React/05-hook-app/src/02-useEffect/Message.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react"
 import { useEffect } from "react"
 
 export const Message = () => {
     
+    const [coords, setCoords] = useState({ x: 0, y: 0})
+
     useEffect(() => {
-        console.log('Message Mounted');
+
+            const onMouseMove = ({ x, y }) => {
+                setCoords({ x, y })
+            }
+
+            window.addEventListener( 'mousemove', onMouseMove);
 
         return () => {
-            console.log('Message UnMounted');
+            window.removeEventListener( 'mousemove', onMouseMove);
         }
     }, [])
   
     return (
         <>
             <h3>Usuario ya existe</h3>
+            { JSON.stringify( coords )}
         </>
     )
 }
