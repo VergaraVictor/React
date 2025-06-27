22c87bca8639198676a9414f70d3164779ce7951
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-06-27 08:22:20 -0500

    FormularioConCustomHook

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
 import './index.css';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/SimpleWithCustomHook';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
