1b1d401ee42666932420eefc7b465bbda86fd40c
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-01 08:09:08 -0500

    useFetchCustomHook

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './02-useEffect/SimpleWithCustomHook';
+// import { FormWithCustomHook } from './02-useEffect/SimpleWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>,
 )
