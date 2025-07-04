ce93cf28660ee0d256f11b669630dfcf4e46680c
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-04 11:08:16 -0500

    useLayoutEffect

---
+++ React/05-hook-app/src/03-examples/index.js
@@ -0,0 +1,5 @@
+
+
+export * from './MultipleCustomHooks';
+export * from './LoadingMessage';
+export * from './PokemonCard';
--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -7,11 +7,13 @@ import './index.css';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/SimpleWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <Layout />
   </React.StrictMode>,
 )
