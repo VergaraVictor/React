6f3532ff956922d2d1394eef1d42c133ec8cadbe
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-05 07:52:30 -0500

    MemoMetodoDeReact

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -8,12 +8,13 @@ import './index.css';
 // import { FormWithCustomHook } from './02-useEffect/SimpleWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <FocusScreen /> */}
-    <Layout />
+    <Memorize />
   </React.StrictMode>,
 )
