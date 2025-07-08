ee89a2d459889610a657df5018d4de3dab5bb25c
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-08 08:08:00 -0500

    useCallback

--- React/05-hook-app/src/06-memos/CallbackHook.jsx
+++ React/05-hook-app/src/06-memos/CallbackHook.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react"
-import { ShowIncrement } from "./ShowIncrement";
+import { useCallback, useState, useEffect } from 'react';
+import { ShowIncrement } from './ShowIncrement';
 
 export const CallbackHook = () => {
     const [counter, setCounter] = useState( 10 );
 
-    const incrementFather = () => {
-        setCounter( counter + 1);
-    }
+    const incrementFather = useCallback(
+        () => {      
+            setCounter( (value) => value + 1);
+        },
+        [counter],
+    );
+
+    useEffect(() => {
+        // incrementFather();
+    }, [incrementFather])
+
+    // const incrementFather = () => {
+    //     setCounter( counter + 1);
+    // }
     
     return (
     <div>
--- React/05-hook-app/src/06-memos/ShowIncrement.jsx
+++ React/05-hook-app/src/06-memos/ShowIncrement.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
 
-export const ShowIncrement = ({ increment }) => {
+export const ShowIncrement = React.memo( ({ increment }) => {
     
+    console.log('Me volví a generar :( ');
+
     return (
         <button
             className="btn btn-primary"
@@ -11,5 +14,5 @@ export const ShowIncrement = ({ increment }) => {
             Incrementar
         </button>
     )
-}
+})
 
