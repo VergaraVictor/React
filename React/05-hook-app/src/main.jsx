6513c3fda6ed32c385d288d190a27137f4f0ef67
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-10 07:25:13 -0500

    ContinuacionDelProyectoHookApp

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -14,9 +14,10 @@ import './index.css';
 // import { CallbackHook } from './06-memos/CallbackHook';
 import { Padre } from './07-tarea-memo/Padre';
 
+import './08-useReducer/intro-reducer';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Padre />
+//   </React.StrictMode>,
+// )
