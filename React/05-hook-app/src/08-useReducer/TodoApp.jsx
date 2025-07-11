700bf4eca0e6818767fb5f0698b184b9847efff4
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-11 08:49:18 -0500

    CreandoElCascaronDeLasListasdeTODOs

--- React/05-hook-app/src/08-useReducer/TodoApp.jsx
+++ React/05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -9,7 +9,7 @@ const initialState = [
     },
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false,
     }
 ]
@@ -18,16 +18,56 @@ export const TodoApp = () => {
     
     const [ todos, dispatch ] = useReducer( todoReducer, initialState )
     
+    const handleNewTodo = ( todo ) => {
+        console.log({ todo });
+    }
+
     return (
     <>
-        <h1>TodoApp</h1>
+        <h1>TodoApp: 10, <small>pendientes: 2</small> </h1>
         <hr />
 
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+        <div className='row'>
+            <div className='col-7'>
+
+                {/* TodoList */}
+                <ul className='list-group'>
+                    {
+                        todos.map( todo => (
+                            // TodoItem ....
+                            <li key={ todo.id } className='list-group-item d-flex justify-content-between'>
+                                <span className='align-self-center'>Item 1</span>
+                                <button className='btn btn-danger'>Borrar</button>
+                            </li>
+                        ))    
+                    }
+                </ul>
+                {/* Fin TodoList */}
+            </div>
+
+            <div className='col-5'>
+                <h4>Agrgar TODO</h4>
+                <hr />
+                {/* TodoAdd onNewTodo( todo ) */}
+                {/* { id: new Date()..., description: '', done: false } */}
+                <form>
+                    <input 
+                        type="text"
+                        placeholder='¿Qué hay que hacer?'
+                        className='form-control' 
+                    />
+
+                    <button
+                        type='submit'
+                        className='btn btn-outline-primary mt-1'
+                    >
+                        Agregar
+                    </button>
+                </form>
+                {/* Fin TodoAdd */}
+            </div>
+        </div>
+
     </>
     )
 }
