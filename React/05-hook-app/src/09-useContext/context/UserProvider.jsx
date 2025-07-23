9c29aab821e62d944583432bcf6ff9d6b3f5d89e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-23 08:34:55 -0500

    CreateContextYContextProvider

---
+++ React/05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,17 @@
+import { UserContext } from "./UserContext"
+
+const user = {
+    id: 123,
+    name: 'victor Vergara',
+    email: 'victorvergara@vergara.com'
+}
+
+
+export const UserProvider = ({ children }) => {
+  return (
+    <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+        { children }
+    </UserContext.Provider>
+  )
+}
+
