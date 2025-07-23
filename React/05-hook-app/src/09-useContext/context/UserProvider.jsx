8cb8d134b16ed56b27a88d9b1a264b380bbe7763
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-23 09:06:52 -0500

    useContext

--- React/05-hook-app/src/09-useContext/context/UserProvider.jsx
+++ React/05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: 123,
-    name: 'victor Vergara',
-    email: 'victorvergara@vergara.com'
-}
+// const user = {
+//     id: 123,
+//     name: 'victor Vergara',
+//     email: 'victorvergara@vergara.com'
+// }
 
 
 export const UserProvider = ({ children }) => {
-  return (
-    <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+
+    const [user, setUser] = useState();
+    
+    return (
+    // <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+    <UserContext.Provider value={{ user, setUser }}>
         { children }
     </UserContext.Provider>
-  )
+    )
 }
 
