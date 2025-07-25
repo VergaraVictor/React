78c18883de49c169b9e0fcc048f9c6c9d83d401f
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-25 08:09:46 -0500

    EjecutarFuncionesDelCustomHookDentroDeLasPruebas

--- React/05-hook-app/tests/hooks/useCounter.test.js
+++ React/05-hook-app/tests/hooks/useCounter.test.js
@@ -18,10 +18,53 @@ describe('Pruebas en el useCounter', () => {
     test('Debe generar el counter con el valor de 100 ', () => {
         
         const { result } = renderHook( () => useCounter(100) );
-        const { counter, decrement, increment, reset } = result.current;
+        const { counter } = result.current;
+        expect( counter ).toBe(100);
+
+    });
+
+    test('debe de incrementar el contador ', () => {
+
+        const { result } = renderHook( () => useCounter(100) );
+        const { counter, increment } = result.current;
+        
+        act( () => {
+            increment();
+            increment(2);
+        });
+
+        expect( result.current.counter ).toBe(103);
+
+    });
+
+    test('debe de decrementar el contador ', () => {
+
+        const { result } = renderHook( () => useCounter(100) );
+        const { counter, decrement } = result.current;
+        
+        act( () => {
+            decrement();
+            decrement(2);
+        });
+
+        expect( result.current.counter ).toBe(97);
+
+    });
+
+    test('debe de realizar el reset ', () => {
+
+        const { result } = renderHook( () => useCounter(100) );
+        const { counter, decrement, reset } = result.current;
+        
+        act( () => {
+            decrement();
+            reset();
+        });
+
         expect( result.current.counter ).toBe(100);
 
     })
+    
     
 
 });
