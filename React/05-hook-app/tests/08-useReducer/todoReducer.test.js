7ada185a527a1105a8e5f105331796959a530b13
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-07-31 08:00:42 -0500

    ResolucionDeLaTarea

--- React/05-hook-app/tests/08-useReducer/todoReducer.test.js
+++ React/05-hook-app/tests/08-useReducer/todoReducer.test.js
@@ -35,16 +35,28 @@ describe('pruebas en todoReducer', () => {
 
     test('debe eliminar un todo', () => {
         
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: 1
+        };
 
+        const newState = todoReducer( initialState, action );
+        expect( newState.length ).toBe( 0 );
 
     });
 
     test('debe de realizar el Toggle del todo', () => {
         
-        
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: 1
+        };
 
-    })
-    
-    
+        const newState = todoReducer( initialState, action );
+        expect( newState[0].done ).toBe( true );
+
+        const newState2 = todoReducer( newState, action );
+        expect( newState2[0].done ).toBe( false );
+    });
     
 });
