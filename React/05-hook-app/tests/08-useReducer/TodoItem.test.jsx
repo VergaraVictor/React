1b246f8eba9e72780f826eb631cd8b86c3d85bc4
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-08-04 08:30:48 -0500

    PruebasEnElComponenteTodoItem

---
+++ React/05-hook-app/tests/08-useReducer/TodoItem.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { TodoItem } from "../../src/08-useReducer/TodoItem";
+
+describe('Pruebas en <TodoItem />', () => {
+
+    const todo = {
+        id: 1,
+        description: 'Piedra del Alma',
+        done: false
+    };
+
+    const onDeleteTodoMock = jest.fn();
+    const onToggleTodoMock = jest.fn();
+
+    beforeEach( () => jest.clearAllMocks() );
+
+    test('debe de mostrar Todo Pendiente de completar', () => {
+        
+        render( 
+            <TodoItem 
+                todo={ todo } 
+                onToggleTodo={ onToggleTodoMock } 
+                onDeleteTodoMock={ onDeleteTodoMock } 
+            />
+        );
+
+        const liElement = screen.getByRole('listitem');
+        console.log(liElement.innerHTML);
+
+        expect ( liElement.className ).toBe('list-group-item d-flex justify-content-between');
+
+        const spanElement = screen.getByLabelText('span');
+        expect( spanElement.className ).toContain('align-self-center');
+
+        // screen.debug();
+
+    });
+});
