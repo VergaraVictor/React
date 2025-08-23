d91c71d452d500fad79f71530e8a2f769e470c0c
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-08-23 07:38:27 -0500

    01-VariablesYConstantes

---
+++ React-2025/01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+
+const firsName: string = 'Victor';
+let lastName = 'Vergara';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLettetH = lastName.includes('V')
+
+console.log({ containsLettetH, diceNumber, firsName, lastName });
