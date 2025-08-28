c39b29c4aec04702d4c1f671d63047f9749c9cca
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-08-28 08:10:52 -0500

    FuncionesConMultiplesRetornos

--- React-2025/01-reforzamiento/src/bases/05-functions.ts
+++ React-2025/01-reforzamiento/src/bases/05-functions.ts
@@ -2,32 +2,60 @@
 
 function greet( name: string ):string {
     return `Hola ${ name}`
-}
+};
 
-const greet2 = (name:string):string => {
-    return `Hola ${ name}`
-}
+// const greet2 = (name:string):string => {
+//     return `Hola ${ name}`
+// }
+
+const greet2 = (name:string):string => `Hola ${ name}`;
 
 const message = greet('Goku');
 const message2 = greet2('Vegeta');
 
 console.log(message, message2);
 
-function getUser() {
-    return {
-        uid: 'ABC-123',
-        username: 'El_Papi23'
-    }
+interface User {
+    uid: string;
+    username: string;
 }
 
-const getUser2 = () => {
+function getUser(): User {
     return {
         uid: 'ABC-123',
         username: 'El_Papi23'
     }
-}
+};
+
+// const getUser2 = () => {
+//     return {
+//         uid: 'ABC-123',
+//         username: 'El_Papi23'
+//     }
+// };
+
+const getUser2 = (): User => ({
+    uid: 'ABC-123',
+    username: 'El_Papi23'
+});
 
 const user = getUser()
 const user2 = getUser2() 
 console.log(user, user2);
+
+const myNumbers: number[] = [1,2,3,4,5];
+
+// myNumbers.forEach( function(value) {
+//     console.log({ value });
+// });
+
+// myNumbers.forEach((value) => {
+//     console.log(value);
+// });
+
+// myNumbers.forEach((value, index, arr) => {
+//     console.log(value, index, arr);
+// });
+
+myNumbers.forEach(console.log);
 
