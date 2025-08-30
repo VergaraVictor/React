d7ad99676ac0ddc9f4993ff7d4388d6acc0c23ce
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-08-30 09:48:00 -0500

    DesestructuracionDeObjetosparte1

---
+++ React-2025/01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -0,0 +1,14 @@
+
+const person = {
+    name: 'Tony',
+    age: 45,
+    key: 'Ironman',
+};
+
+// const name = person.name;
+// const age = person.age;
+// const key = person.key;
+
+const {} = person;
+
+console.log( {name, age, key} );
