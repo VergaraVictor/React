7397f35a6367719cb4ab3ac8663772aed3d75250
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-01 09:30:22 -0500

    DesestructuracionDeArreglos

---
+++ React-2025/01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -0,0 +1,16 @@
+const characterNames = ['Goku', 'Vegeta', 'Trunks'];
+
+const [ , , trunks ] = characterNames;
+
+console.log({ trunks });
+
+
+const returnsArrayFn = () => {
+    return ['ABC', 123] as const;
+}
+
+const [ letters, numbers ] = returnsArrayFn();
+
+console.log(letters, numbers);
+
+
