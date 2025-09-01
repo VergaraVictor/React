d6f729296a0c67e3f94224edf4a4305a6bda6316
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-01 09:17:21 -0500

    DesestructuracioDeObjetos

--- React-2025/01-reforzamiento/src/bases/06-obj-destructuring.ts
+++ React-2025/01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -9,6 +9,36 @@ const person = {
 // const age = person.age;
 // const key = person.key;
 
-const {} = person;
+const {   key, name:ironmanNAme, age  } = person;
 
-console.log( {name, age, key} );
+console.log( {ironmanNAme, age, key} );
+
+interface Hero {
+    name: string;
+    age: number; 
+    key: string;
+    rank?: string;
+}
+
+const useContext = ({key, name, age, rank = 'sin rango' }: Hero ) => {
+
+    return {
+        keyname: key,
+        user: {
+            name,
+            age,
+        },
+        rank: rank
+    };
+};
+
+const { 
+    rank, 
+    keyname,
+    user, 
+    // user: { name } 
+} = useContext(person);
+const { name } = user;
+
+console.log({ rank, keyname, name });
+
