04217f3a74af3b1f1d099013c1d28dcf666f5bdd
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-03 08:02:24 -0500

    TareaDesestructuracion

--- React-2025/01-reforzamiento/src/bases/07-array-destructuring.ts
+++ React-2025/01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -13,4 +13,17 @@ const [ letters, numbers ] = returnsArrayFn();
 
 console.log(letters, numbers);
 
+// Tarea
 
+const useState = (value: string) => {
+    return [ 
+        value, 
+        (newValue: string) => {
+            console.log(newValue);
+        }, 
+    ] as const;
+};
+
+const [name, setName] = useState('Goku');
+console.log(name); //Goku
+setName('Vegeta'); //Imprime Vegeta
