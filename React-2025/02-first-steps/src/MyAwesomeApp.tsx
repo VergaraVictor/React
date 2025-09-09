6ddc3d7ec569cc72a7f8e2e8754cf736f0b07ce9
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-09 08:04:40 -0500

    TareaSegundoComponente

---
+++ React-2025/02-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,17 @@
+export const MyAwesomeApp = () => {
+    return (
+        <>
+            <h1>Victor</h1>
+            <h3>Vergara</h3>
+        </>
+    );
+};
+
+// export function MyAwesomeApp() {
+//     return (
+//         <>
+//             <h1>Victor</h1>
+//             <h3>Vergara</h3>
+//         </>
+//     );
+// }
