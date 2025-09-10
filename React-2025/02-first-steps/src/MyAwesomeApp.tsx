ca8dcae6cd0b7a66e5e2713de89f9eef1a28989e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-10 09:48:40 -0500

    ImpresionDeVariables

--- React-2025/02-first-steps/src/MyAwesomeApp.tsx
+++ React-2025/02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,28 @@
+const firstName = 'Victor';
+const lastName = 'Vergara';
+
+const favoriteGames = ['Elden ring', 'Smash', 'Metal Gear'];
+const isActive = false;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canadá',
+};
+
 export const MyAwesomeApp = () => {
     return (
         <>
-            <h1>Victor</h1>
-            <h3>Vergara</h3>
+            <h1> {firstName} </h1>
+            <h3> {lastName} </h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{2 + 2}</p>
+
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+            <p>
+                {JSON.stringify(address)}
+            </p>
         </>
     );
 };
