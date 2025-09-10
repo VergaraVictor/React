da4756f8c43cab0fadb5c395c9af8ace1c09e760
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-10 10:02:53 -0500

    ColocarEstilosDeCSS

--- React-2025/02-first-steps/src/MyAwesomeApp.tsx
+++ React-2025/02-first-steps/src/MyAwesomeApp.tsx
@@ -1,3 +1,5 @@
+import type { CSSProperties } from "react";
+
 const firstName = 'Victor';
 const lastName = 'Vergara';
 
@@ -7,6 +9,13 @@ const isActive = false;
 const address = {
     zipCode: 'ABC-123',
     country: 'Canadá',
+};
+
+const myStyles: CSSProperties = {
+    backgroundColor: '#fafafa',
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 30,
 };
 
 export const MyAwesomeApp = () => {
@@ -20,7 +29,7 @@ export const MyAwesomeApp = () => {
 
             <h1>{isActive ? 'Activo' : 'No activo'}</h1>
 
-            <p>
+            <p style={myStyles}>
                 {JSON.stringify(address)}
             </p>
         </>
