488dc951e4d879d29f4b4874572de51f25114628
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-13 08:35:11 -0500

    HookUseState

--- React-2025/02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ React-2025/02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 interface Props {
     name: string;
     quantity?: number;
 }
 
 
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+
+    const [count, setCount] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+
+    const handleSubtract = () => {
+        if (count === 1) return;
+
+        setCount(count - 1);
+    }
 
     const handleClick = () => {
         console.log(`Click en ${name}`);
@@ -28,12 +42,12 @@ export const ItemCounter = ({ name, quantity }: Props) => {
                 onMouseEnter={() => {
                     console.log(`Mouse enter ${name}`);
                 }}
-                onClick={handleClick}
+                onClick={handleAdd}
             >
                 +1
             </button>
-            <span>{quantity}</span>
-            <button>-1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}>-1</button>
         </section>
     );
 };
