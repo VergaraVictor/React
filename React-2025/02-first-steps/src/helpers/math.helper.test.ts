1c19f14192755d3f62f126afadb1c22e4df51c21
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-16 08:09:22 -0500

    AgruparPruebasSimilares

--- React-2025/02-first-steps/src/helpers/math.helper.test.ts
+++ React-2025/02-first-steps/src/helpers/math.helper.test.ts
@@ -1,19 +1,53 @@
-import { expect, test } from 'vitest';
-import { add } from './math.helper';
+import { describe, expect, test } from 'vitest';
+import { add, multiply, subtract } from './math.helper';
 
-
-
-test('shoul add two positives numbers', () => {
-
-    // ! 1. Arrange
-    const a = 1;
-    const b = 2;
-    // ! 2. Act
-    const result = add(a, b);
-    // ! 3. Assert
-    console.log({ result });
-    expect(result).toBe(a + b);
+describe('add', () => {
+    test('shoul add two positives numbers', () => {
+        // ! 1. Arrange
+        const a = 1;
+        const b = 2;
+        // ! 2. Act
+        const result = add(a, b);
+        // ! 3. Assert
+        console.log({ result });
+        expect(result).toBe(a + b);
+    });
 });
+
+describe('subtract', () => {
+    test('shoul substract two positive numbers', () => {
+        const a = 2;
+        const b = 4;
+        const result = subtract(a, b);
+        console.log({ result });
+        expect(result).toBe(a - b);
+    });
+    test('shoul substract two negative numbers', () => {
+        const a = -2;
+        const b = -4;
+        const result = subtract(a, b);
+        console.log({ result });
+        expect(result).toBe(a - b);
+    });
+});
+
+describe('multiply', () => {
+    test('shoul multiply two positive numbers', () => {
+        const a = 2;
+        const b = 4;
+        const result = multiply(a, b);
+        console.log({ result });
+        expect(result).toBe(a * b);
+    });
+    test('shoul multiply two negative numbers', () => {
+        const a = -2;
+        const b = -4;
+        const result = multiply(a, b);
+        console.log({ result });
+        expect(result).toBe(a * b);
+    });
+});
+
 
 
 
