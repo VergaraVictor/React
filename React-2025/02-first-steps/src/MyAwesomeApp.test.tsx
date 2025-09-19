fd4aa01cecde9cbe2886b23dc95ab3c698f834cf
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-19 08:19:17 -0500

    EvaluarSnapshots

--- React-2025/02-first-steps/src/MyAwesomeApp.test.tsx
+++ React-2025/02-first-steps/src/MyAwesomeApp.test.tsx
@@ -32,4 +32,15 @@ describe('MyAwesomeApp', () => {
         const h1 = screen.getByTestId('first-name-title');
         expect(h1.innerHTML).toContain('Victor');
     });
+
+    test('should match snapshot', () => {
+        const { container } = render(<MyAwesomeApp />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should match snapshot', () => {
+        render(<MyAwesomeApp />);
+        expect(screen.getByTestId('div-app')).toMatchSnapshot();
+    });
+
 });
