597d4b30c6cab2939290aaf37984ef69996e6316
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-22 08:16:57 -0500

    PruebasEnFirstStepsApp

---
+++ React-2025/02-first-steps/src/FirstStepsApp.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, test } from "vitest";
+import { FirstStepsApp } from "./FirstStepsApp";
+import { render } from "@testing-library/react";
+
+
+describe('FirstStepsApp', () => {
+
+    test('should match snapshot', () => {
+        const { container } = render(<FirstStepsApp />)
+
+        expect(container).toMatchSnapshot();
+    });
+});
