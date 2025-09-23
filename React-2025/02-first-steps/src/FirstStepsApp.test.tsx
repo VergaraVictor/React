8d42c2802c13ebcacc01bd1bc8e92847585575ac
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-23 08:03:07 -0500

    EsperarArgumentosEspecificos

--- React-2025/02-first-steps/src/FirstStepsApp.test.tsx
+++ React-2025/02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,13 +1,27 @@
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 import { render, screen } from "@testing-library/react";
 
+const mockItemCounter = vi.fn((_props: unknown) => {
+    return (<div data-testid="ItemCounter" />)
+});
+
 vi.mock('./shopping-cart/ItemCounter', () => ({
-    ItemCounter: () => <div data-testid="ItemCounter" />,
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
+
+// vi.mock('./shopping-cart/ItemCounter', () => ({
+//     ItemCounter: (props: unknown) => (
+//         <div data-testid="ItemCounter" name={props.name} quantity={props.quantity} />
+//     ),
+// }));
 
 
 describe('FirstStepsApp', () => {
+
+    afterEach(() => {
+        vi.clearAllMocks()
+    })
 
     test('should match snapshot', () => {
         const { container } = render(<FirstStepsApp />)
@@ -25,6 +39,25 @@ describe('FirstStepsApp', () => {
 
         screen.debug();
 
-    })
+    });
+
+    test('should render ItemCounter with correct props', () => {
+
+        render(<FirstStepsApp />);
+
+        expect(mockItemCounter).toHaveBeenCalledTimes(3);
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Nintendo Switch 2',
+            quantity: 1
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Pro Controller',
+            quantity: 2
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Super Smash',
+            quantity: 5
+        });
+    });
 
 });
