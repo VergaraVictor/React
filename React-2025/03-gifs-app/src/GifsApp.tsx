78ee0f8ef607c1af602f995e68324ed8800767b8
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-09-30 08:26:11 -0500

    ManejoDelComponenteDeBusqueda

--- React-2025/03-gifs-app/src/GifsApp.tsx
+++ React-2025/03-gifs-app/src/GifsApp.tsx
@@ -12,6 +12,10 @@ export const GifsApp = () => {
         console.log({ term });
     };
 
+    const handleSearch = (query: string) => {
+        console.log({ query });
+    };
+
     return (
         <>
             {/* Header */}
@@ -22,7 +26,10 @@ export const GifsApp = () => {
 
             {/* Search */}
             {/* SearchBar */}
-            <SearchBar placeholder="Busca lo que quieras" />
+            <SearchBar
+                placeholder="Busca lo que quieras"
+                onQuery={handleSearch}
+            />
 
             {/* Busquedas Previas */}
             {/* PreviousSearches */}
