9a575d79ba9cde8fefe146d30ef2c33e35e1e8a8
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-10-02 07:53:20 -0500

    TareaVerBusquedasPrevias

--- React-2025/03-gifs-app/src/GifsApp.tsx
+++ React-2025/03-gifs-app/src/GifsApp.tsx
@@ -12,8 +12,19 @@ export const GifsApp = () => {
         console.log({ term });
     };
 
-    const handleSearch = (query: string) => {
-        console.log({ query });
+    const handleSearch = (query: string = '') => {
+
+        // Convertir el query a minúsculas y eliminar espacios en blanco
+        query = query.trim().toLocaleLowerCase();
+        // Validar que el query no esté vacío
+        if (query.length === 0) return;
+        // Evitar búsquedas duplicadas verificando si el término ya existe en previousTerms ( si existe, no hacer nada )
+        if (previousTerms.includes(query)) return;
+        // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
+        // const currentTerms = previousTerms.slice(0.6);
+        // currentTerms.unshift(query)
+
+        setPreviousTerms([query, ...previousTerms].splice(0, 8));
     };
 
     return (
