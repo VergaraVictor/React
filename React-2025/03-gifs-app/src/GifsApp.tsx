4bae05ca8e984e47f5e95b834ee93dbcaf01fc08
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-10-09 07:38:21 -0500

    HookPersonalizadoUseGifs

--- React-2025/03-gifs-app/src/GifsApp.tsx
+++ React-2025/03-gifs-app/src/GifsApp.tsx
@@ -1,38 +1,13 @@
-import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { useGifs } from "./gifs/hooks/useGifs";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions";
-import type { Gif } from "./gifs/interfaces/gif.interface";
+
 
 export const GifsApp = () => {
 
-    const [gifs, setGifs] = useState<Gif[]>([])
-    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-
-    const handleTermClicked = (term: string) => {
-        console.log({ term });
-    };
-
-    const handleSearch = async (query: string = '') => {
-
-        // Convertir el query a minúsculas y eliminar espacios en blanco
-        query = query.trim().toLocaleLowerCase();
-        // Validar que el query no esté vacío
-        if (query.length === 0) return;
-        // Evitar búsquedas duplicadas verificando si el término ya existe en previousTerms ( si existe, no hacer nada )
-        if (previousTerms.includes(query)) return;
-        // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
-        // const currentTerms = previousTerms.slice(0.6);
-        // currentTerms.unshift(query)
-
-        setPreviousTerms([query, ...previousTerms].splice(0, 8));
-
-        const gifs = await getGifsByQuery(query);
-        setGifs(gifs)
-
-    };
+    const { gifs, handleTermClicked, handleSearch, previousTerms } = useGifs();
 
     return (
         <>
---
+++ React-2025/03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { getGifsByQuery } from "../actions/get-gifs-by-query.actions";
+import type { Gif } from "../interfaces/gif.interface";
+
+
+export const useGifs = () => {
+
+    const [gifs, setGifs] = useState<Gif[]>([])
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+    const handleTermClicked = async (term: string) => {
+        const gifs = await getGifsByQuery(term);
+        setGifs(gifs);
+    };
+
+    const handleSearch = async (query: string = '') => {
+
+        // Convertir el query a minúsculas y eliminar espacios en blanco
+        query = query.trim().toLocaleLowerCase();
+        // Validar que el query no esté vacío
+        if (query.length === 0) return;
+        // Evitar búsquedas duplicadas verificando si el término ya existe en previousTerms ( si existe, no hacer nada )
+        if (previousTerms.includes(query)) return;
+        // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
+        // const currentTerms = previousTerms.slice(0.6);
+        // currentTerms.unshift(query)
+
+        setPreviousTerms([query, ...previousTerms].splice(0, 8));
+
+        const gifs = await getGifsByQuery(query);
+        setGifs(gifs)
+    };
+
+    return {
+        // props
+        gifs,
+
+        // Methods
+        handleSearch,
+        handleTermClicked,
+        previousTerms,
+    };
+};
+
