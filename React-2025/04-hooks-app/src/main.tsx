8518245b694648fd46a9b95bccf0482452866744
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-11-13 08:07:08 -0500

    useEffectDispararEfectosSecundariosV1

--- React-2025/04-hooks-app/src/main.tsx
+++ React-2025/04-hooks-app/src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 
 // import { HooksApp } from './HooksApp'
 
-import './index.css'
-import { TrafficLight } from './01-useState/TrafficLight'
+import './index.css';
+import { TrafficLight } from './01-useState/TrafficLight';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    <Traff />
   </StrictMode>,
 )
