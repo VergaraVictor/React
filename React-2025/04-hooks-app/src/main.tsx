4179a09da82d3d28879bd4c3b372a97d0d0e1cf5
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-11-20 07:35:16 -0500

    TareaCustomHook

--- React-2025/04-hooks-app/src/main.tsx
+++ React-2025/04-hooks-app/src/main.tsx
@@ -4,13 +4,15 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css';
+import { TrafficLightWithEffectWithHook } from './02-useEffect/TrafficLightWithEffectWithHook';
 // import { TrafficLight } from './01-useState/TrafficLight';
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
-    <TrafficLightWithEffect />
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithEffectWithHook />
   </StrictMode>,
 )
