0946a916ad51096c789d67db91d0f8bb1f02e026
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-11-20 07:58:05 -0500

    SolucionDeTareaCustomHook

--- React-2025/04-hooks-app/src/02-useEffect/TrafficLightWithEffectWithHook.tsx
+++ React-2025/04-hooks-app/src/02-useEffect/TrafficLightWithEffectWithHook.tsx
@@ -1,57 +1,9 @@
-// import { useEffect, useState } from "react";
-
-// const colors = {
-//     red: 'bg-red-500 animate-pulse',
-//     yellow: 'bg-yellow-500 animate-pulse',
-//     green: 'bg-green-500 animate-pulse',
-// };
-
-// type TrafficLightColor = keyof typeof colors;
+import { useTrafficLight } from "../hooks/useTrafficLight";
 
 export const TrafficLightWithEffectWithHook = () => {
 
-    const { } = useTrafficLight();
+    const { countdown, percentage, redLight, greenLight, yellowLight, } = useTrafficLight();
 
-    // const [light, setlight] = useState<TrafficLightColor>('red');
-    // const [countdown, setCountdown] = useState(5);
-
-    // // Countdown Effect
-    // useEffect(() => {
-    //     // console.log({ countdown })
-    //     if (countdown === 0) return;
-
-    //     const intervalId = setInterval(() => {
-    //         setCountdown(prev => prev - 1);
-    //     }, 1000);
-
-    //     return () => {
-    //         clearInterval(intervalId);
-    //     };
-
-    // }, [countdown]);
-
-    // // Change light color effect
-    // useEffect(() => {
-    //     if (countdown > 0) return;
-
-
-    //     setCountdown(5);
-
-    //     if (light === 'red') {
-    //         setlight('green');
-    //         return;
-    //     }
-
-    //     if (light === 'yellow') {
-    //         setlight('red');
-    //         return;
-    //     }
-
-    //     if (light === 'green') {
-    //         setlight('yellow');
-    //         return;
-    //     }
-    // }, [countdown, light])
 
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -65,14 +17,14 @@ export const TrafficLightWithEffectWithHook = () => {
                 <div className="w-64 bg-gray-700 rounded-full h-2">
                     <div
                         className="bg-blue-500 h-2 rounder-full transition-all duration-1000 ease-linear"
-                        style={{ width: `${(countdown / 5) * 100}%` }}
+                        style={{ width: `${percentage}%` }}
                     ></div>
                 </div>
 
-                <div className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${redLight} rounded-full`}></div>
 
-                <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
-                <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${yellowLight} rounded-full`}></div>
+                <div className={`w-32 h-32 ${greenLight} rounded-full`}></div>
 
             </div>
         </div>
---
+++ React-2025/04-hooks-app/src/hooks/useTrafficLight.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+};
+
+type TrafficLightColor = keyof typeof colors;
+
+export const useTrafficLight = () => {
+
+    const [light, setlight] = useState<TrafficLightColor>('red');
+    const [countdown, setCountdown] = useState(5);
+
+    // Countdown Effect
+    useEffect(() => {
+        // console.log({ countdown })
+        if (countdown === 0) return;
+
+        const intervalId = setInterval(() => {
+            setCountdown(prev => prev - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+
+    }, [countdown]);
+
+    // Change light color effect
+    useEffect(() => {
+        if (countdown > 0) return;
+
+
+        setCountdown(5);
+
+        if (light === 'red') {
+            setlight('green');
+            return;
+        }
+
+        if (light === 'yellow') {
+            setlight('red');
+            return;
+        }
+
+        if (light === 'green') {
+            setlight('yellow');
+            return;
+        }
+    }, [countdown, light])
+
+    return {
+        // Props
+        countdown,
+        light,
+        colors,
+
+        // Computed
+        percentage: (countdown / 5) * 100,
+        greenLight: light === 'green' ? colors.green : 'bg-gray-500',
+        redLight: light === 'red' ? colors.red : 'bg-gray-500',
+        yellowLight: light === 'yellow' ? colors.yellow : 'bg-gray-500',
+
+        // Methods
+
+    };
+}
+
