a0ada523ee109dd82e5407b5aa8da470419f8407
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-11-25 07:56:27 -0500

    conectarVariosCustomHooksEntreSiVersion final

--- React-2025/04-hooks-app/src/hooks/usePokemon.tsx
+++ React-2025/04-hooks-app/src/hooks/usePokemon.tsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
+
+interface Pokemon {
+    id: number;
+    name: string;
+    imageUrl: string;
+}
+
 interface Props {
     id: number,
 }
 
 export const usePokemon = ({ id }: Props) => {
 
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
+    const getPokemonById = async (id: number) => {
+
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+        const data = await response.json();
+
+        setPokemon({
+            id: id,
+            name: data.name,
+            imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        });
+    };
+
+    useEffect(() => {
+
+        fetch('...')
+
+    }, []);
 
     return {
-
+        // Properties
+        pokemon,
     }
 }
 
