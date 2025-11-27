1c45bba757da0d1c22772448c4f39cbd4547a584
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-11-27 07:43:13 -0500

    Parte2ConectarVariosCustomHooks

--- React-2025/04-hooks-app/src/hooks/usePokemon.tsx
+++ React-2025/04-hooks-app/src/hooks/usePokemon.tsx
@@ -13,10 +13,12 @@ interface Props {
 export const usePokemon = ({ id }: Props) => {
 
     const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     const getPokemonById = async (id: number) => {
+        setIsLoading(true);
 
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await response.json();
 
         setPokemon({
@@ -24,17 +26,22 @@ export const usePokemon = ({ id }: Props) => {
             name: data.name,
             imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
         });
+
+        setIsLoading(false);
     };
 
     useEffect(() => {
 
-        fetch('...')
+        getPokemonById(id)
 
-    }, []);
+    }, [id]);
 
     return {
         // Properties
+        isLoading,
         pokemon,
+
+        formattedId: id.toString().padStart(3, '0'),
     }
 }
 
