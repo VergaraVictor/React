9a8e48d8f828c73f643b79af28e9076f40b25b58
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-12-12 08:01:03 -0500

    ControlarEstadosComplejosUseReducer

--- React-2025/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ React-2025/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -19,6 +19,14 @@ export type TaskAction =
     | { type: 'TOGGLE_TODO', payload: number }
     | { type: 'DELETE_TODO', payload: number };
 
+export const getTaskInitialState = (): TaskState => {
+    return {
+        todos: [],
+        completed: 0,
+        pending: 0,
+        length: 0,
+    };
+};
 
 export const taskReducer = (state: TaskState, action: TaskAction): TaskState => {
 
