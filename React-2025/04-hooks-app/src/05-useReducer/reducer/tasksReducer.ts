ab6897ae14eaadde9b1b782c169c22c1358fbcc5
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-12-13 08:19:46 -0500

    PersistenciaLocalLocalStorage

--- React-2025/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ React-2025/04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -20,12 +20,20 @@ export type TaskAction =
     | { type: 'DELETE_TODO', payload: number };
 
 export const getTaskInitialState = (): TaskState => {
-    return {
-        todos: [],
-        completed: 0,
-        pending: 0,
-        length: 0,
-    };
+
+    const localStorageState = localStorage.getItem('tasks-state');
+
+    if (!localStorageState) {
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0,
+        };
+    }
+
+    // ! Cuidado ppor que el objeto puede haber sido manipulado.
+    return JSON.parse(localStorageState);
 };
 
 export const taskReducer = (state: TaskState, action: TaskAction): TaskState => {
