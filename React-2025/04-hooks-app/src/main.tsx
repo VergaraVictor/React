7b148fd02ba20032dc442b23e7b92fd4d2573d73
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-01-21 07:39:11 -0500

    useMemoMemorizacionDeValores

--- React-2025/04-hooks-app/src/main.tsx
+++ React-2025/04-hooks-app/src/main.tsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css';
-import { MemoHook } from './06-memos/MemoHook';
+import { MemoCounter } from './06-memos/MemoCounter';
+// import { MemoHook } from './06-memos/MemoHook';
 // import { ScrambleWords } from './05-useReducer/reducer/ScrambleWords';
 // import { TasksApp } from './05-useReducer/TaskApp';
 // import { FocusScreen } from './04-useRef/FocusScreen';
@@ -23,6 +24,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
     {/* <ScrambleWords /> */}
-    <MemoHook />
+    {/* <MemoHook /> */}
+    <MemoCounter />
   </StrictMode>,
 )
