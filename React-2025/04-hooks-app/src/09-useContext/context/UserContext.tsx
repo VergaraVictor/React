d44c87cb8087d5de112c7f672722e605db4e54bb
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-02-02 07:56:04 -0500

    ContextoDeUsuarioInformacionGlobal

--- React-2025/04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ React-2025/04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,14 +1,45 @@
-import { type PropsWithChildren } from "react";
+import { type PropsWithChildren, createContext, useState, } from "react";
+import type { User } from "../data/user-mock.data";
 
 // interface UserContextProps {
 //     children: React.ReactNode;
 // }
 
+type AuthStatus = 'checking' | 'autenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    // state
+    authStatus: AuthStatus;
+    user: User | null
+
+    //methods
+    login: (userId: number) => boolean;
+    logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps)
+
+
+// HOC higher-order component 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
 
-    return
-    <>
-        {children}
-    </>;
+    const [AuthStatus, setAuthStatus] = useState<AuthStatus>('checking')
+    const [user, setUser] = useState<User | null>(null)
+
+    const handleLogin = (userId: number) => {
+        console.log({ userId });
+        return true;
+    }
+
+    const handleLogout = () => {
+        console.log('logout');
+    }
+
+    return <UserContext value={{
+        authStatus: AuthStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout
+    }}>{children}</UserContext >;
 };
 
