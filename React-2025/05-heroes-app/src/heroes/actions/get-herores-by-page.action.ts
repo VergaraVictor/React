449ed6a11f5c70dcbd455d82f7ba31d393a610ce
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-03-18 08:09:32 -0500

    TanStackQueryGestorDeEstadoAsíncrono

--- React-2025/05-heroes-app/src/heroes/actions/get-herores-by-page.action.ts
+++ React-2025/05-heroes-app/src/heroes/actions/get-herores-by-page.action.ts
@@ -2,8 +2,10 @@ import { heroApi } from "../api/hero.api"
 
 
 
-export const getHeroesByPage = async () => {
+export const getHeroesByPageAction = async () => {
     const { data } = await heroApi.get('/')
+
+    console.log({ data });
 
     return data;
 };
