e7a33335199122872ee75e7eaf1d651759409e2e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-03-27 07:37:31 -0500

    QueryParametersSobreuseState

--- React-2025/05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ React-2025/05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -1,18 +1,25 @@
+import { useState } from "react"
+import { useSearchParams } from "react-router"
+import { useQuery } from "@tanstack/react-query";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
-import { useEffect, useState } from "react"
 import { CustomPagination } from "@/components/custom/CustomPagination"
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
 import { getHeroesByPageAction } from "@/heroes/actions/get-herores-by-page.action"
-import { useQuery } from "@tanstack/react-query";
 
 export const HomePage = () => {
 
-  const [activeTab, setActiveTab] = useState<
-    'all' | 'favorites' | 'heroes' | 'villains'
-  >('all');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const activeTab = searchParams.get('tab') ?? 'all';
+
+  const selectedTab = useMemo(() => {
+    const validTabs = ['all', 'favorites', 'heroes', 'villains'];
+    return validTabs.includes(activeTab) ? activeTab : 'all';
+  }, [activeTab]);
 
   const { data: heroesResponse } = useQuery({
     queryKey: ['heroes'],
@@ -39,16 +46,53 @@ export const HomePage = () => {
         <HeroStats />
 
         {/* Tabs */}
-        <Tabs value={activeTab} className="mb-8">
+        <Tabs value={selectedTab} className="mb-8">
           <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger value="all" onClick={() => setActiveTab('all')}>
+            <TabsTrigger
+              value="all"
+              onClick={() =>
+                setSearchParams((prev) => {
+                  prev.set('tab', 'all')
+                  return prev;
+                })
+              }
+            >
               All Characters (16)
             </TabsTrigger>
-            <TabsTrigger value="favorites" className="flex items-center gap-2" onClick={() => setActiveTab('favorites')}>
+            <TabsTrigger
+              value="favorites"
+              className="flex items-center gap-2"
+              onClick={() =>
+                setSearchParams((prev) => {
+                  prev.set('tab', 'favorites')
+                  return prev;
+                })
+              }
+            >
               Favorites (3)
             </TabsTrigger>
-            <TabsTrigger value="heroes" onClick={() => setActiveTab('heroes')}>Heroes (12)</TabsTrigger>
-            <TabsTrigger value="villains" onClick={() => setActiveTab('villains')}>Villains (2)</TabsTrigger>
+            <TabsTrigger
+              value="heroes"
+              onClick={() =>
+                setSearchParams((prev) => {
+                  prev.set('tab', 'heroes')
+                  return prev;
+                })
+              }
+            >
+              Heroes (12)
+            </TabsTrigger>
+            <TabsTrigger
+              value="villains"
+              onClick={() =>
+                setSearchParams((prev) => {
+                  prev.set('tab', 'villains')
+                  return prev;
+                })
+              }
+            >
+              Villains (2)
+            </TabsTrigger>
           </TabsList>
 
           <TabsContent value="all">
