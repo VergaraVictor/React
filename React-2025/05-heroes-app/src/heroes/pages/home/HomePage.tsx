2646aedbbb249dfa5d3c571c3b62926043be13a4
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-03-28 08:49:34 -0500

    ComponentedePaginacion

--- React-2025/05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ React-2025/05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -24,10 +24,10 @@ export const HomePage = () => {
   }, [activeTab]);
 
   const { data: heroesResponse } = useQuery({
-    queryKey: ['heroes'],
+    queryKey: ['heroes', { page, limit }],
     queryFn: () => getHeroesByPageAction(+page, +limit),
     staleTime: 1000 * 60 * 5, // 5 minutos
-  })
+  });
 
   // useEffect(() => {
   //   getHeroesByPage().then();
@@ -119,7 +119,7 @@ export const HomePage = () => {
         </Tabs>
 
         {/* Pagination */}
-        <CustomPagination totalPages={8} />
+        <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
       </>
     </>
   )
