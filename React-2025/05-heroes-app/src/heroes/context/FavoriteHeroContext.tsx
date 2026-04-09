0978e967d3bd5a297fcd6c1a279604be2b3c6f5b
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-04-09 11:08:10 -0500

    ContextoDeFavoritosParte2

--- React-2025/05-heroes-app/src/heroes/context/FavoriteHeroContext.tsx
+++ React-2025/05-heroes-app/src/heroes/context/FavoriteHeroContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, type PropsWithChildren } from "react";
+import { createContext, useEffect, useState, type PropsWithChildren } from "react";
 import type { Hero } from "../types/hero.interface";
 
 interface FavoriteHeroContextValue {
@@ -13,9 +13,14 @@ interface FavoriteHeroContextValue {
 
 export const FavoriteHeroContext = createContext({} as FavoriteHeroContextValue);
 
+const getFavoritesFromLocalStorage = (): Hero[] => {
+    const favorites = localStorage.getItem('favorite');
+    return favorites ? JSON.parse(favorites) : [];
+};
+
 export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
 
-    const [favorites, setFavorites] = useState<Hero[]>([]);
+    const [favorites, setFavorites] = useState<Hero[]>(getFavoritesFromLocalStorage());
 
     const toogleFavorite = (hero: Hero) => {
         const heroExist = favorites.find((h) => h.id === hero.id);
@@ -29,14 +34,22 @@ export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
         setFavorites([...favorites, hero]);
     }
 
+    const isFavorite = (hero: Hero) => {
+        return favorites.some((h) => h.id === hero.id);
+    };
+
+    useEffect(() => {
+        localStorage.setItem('favorite', JSON.stringify(favorites));
+    }, [favorites]);
+
     return (
         <FavoriteHeroContext
             value={{
                 //state
-                favoriteCount: 0,
-                favorites: [],
+                favoriteCount: favorites.length,
+                favorites: favorites,
                 //Methods
-                isFavorite: () => { },
+                isFavorite: isFavorite,
                 toogleFavorite: toogleFavorite,
             }}
         >
