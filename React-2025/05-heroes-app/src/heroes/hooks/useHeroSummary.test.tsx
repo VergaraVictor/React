c4778ff680aa3f37a6df62a6788b410e4d01b874
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-05-16 08:07:25 -0500

    TestUseHeroSummary

---
+++ React-2025/05-heroes-app/src/heroes/hooks/useHeroSummary.test.tsx
@@ -0,0 +1,38 @@
+import type { PropsWithChildren } from "react";
+import { describe, expect, test } from "vitest";
+import { renderHook } from '@testing-library/react';
+import { useHeroSummary } from "./useHeroSummary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+describe('useHeroSummary', () => {
+    
+    const tanStackCustomProvider = () => {
+
+        const queryClient = new QueryClient({
+            defaultOptions: {
+                queries: {
+                    retry: false,
+                },
+            },
+        });
+
+        return ({children}: PropsWithChildren) => (
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        );
+    };
+
+    test('should return the initial state (isLoading)', () => {
+        
+        const { result } = renderHook( () => useHeroSummary(), {
+            wrapper: tanStackCustomProvider()
+        });
+
+        expect(result.current.isLoading).toBe(true);
+        expect(result.current.isError).toBe(false);
+        expect(result.current.data).toBe(undefined);
+        expect(result.current.data).toBeUndefined();
+        
+
+    });
+});
