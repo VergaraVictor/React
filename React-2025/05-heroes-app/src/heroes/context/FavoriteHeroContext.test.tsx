3d4e2630426665c46d831405aaa0a016071fea6f
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-06-02 08:44:43 -0500

    Parte3TestFavoriteHeroContext

--- React-2025/05-heroes-app/src/heroes/context/FavoriteHeroContext.test.tsx
+++ React-2025/05-heroes-app/src/heroes/context/FavoriteHeroContext.test.tsx
@@ -1,7 +1,7 @@
 
 import { use } from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 import { FavoriteHeroContext, FavoriteHeroProvider } from "./FavoriteHeroContext";
 import type { Hero } from '../types/hero.interface';
 
@@ -50,6 +50,9 @@ const renderContextTest = () => {
 }
 
 describe('FavoriteHeroContext', () => {
+    beforeEach(() => {
+        localStorage.clear();
+    });
 
     test('should initialize with default values', () => {
         
@@ -75,4 +78,22 @@ describe('FavoriteHeroContext', () => {
         expect(screen.getByTestId('favorites-list').children.length).toBe(1);
     }); 
 
+    test('should remove hero from favorites when toggleFavorite is called', () => {   
+        localStorage.setItem('favorites', JSON.stringify([mockHero]));
+
+        renderContextTest();
+        expect(screen.getByTestId('is-favorite').textContent).toBe('true');
+        expect(screen.getByTestId('hero-1').textContent).toBe('batman');
+        expect(localStorage.getItem('favorites')).toBe('[{"id":"1","name":"batman"}]');
+        expect(screen.getByTestId('favorite-count').textContent).toBe('1');
+        expect(screen.getByTestId('favorites-list').children.length).toBe(1);
+
+        const button = screen.getByTestId('toggle-favorite');
+        fireEvent.click(button);
+
+        expect(screen.getByTestId('is-favorite').textContent).toBe('false');
+        expect(screen.queryByTestId('hero-1')).toBeNull();
+        expect(screen.getByTestId('favorite-count').textContent).toBe('0');
+        expect(screen.getByTestId('favorites-list').children.length).toBe(0);
+    }); 
 });
