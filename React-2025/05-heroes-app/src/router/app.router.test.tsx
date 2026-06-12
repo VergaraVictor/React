bc23f6bab026949a12d6824a4fa5cda512f266b8
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-06-12 08:50:52 -0500

    MemoryRouterEstablecerRutasYArgumentosVersion1

--- React-2025/05-heroes-app/src/router/app.router.test.tsx
+++ React-2025/05-heroes-app/src/router/app.router.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, test, vi } from 'vitest';
 import { appRouter } from './app.router';
 import { render, screen } from '@testing-library/react';
-import { Outlet, RouterProvider } from 'react-router';
+import { createMemoryRouter, Outlet, RouterProvider, useParams } from 'react-router';
 
 vi.mock('@/heroes/layouts/HeroesLayout', () => ({
     HeroesLayout: () => <div data-testid="heroes-layout">
@@ -13,6 +13,15 @@ vi.mock('@/heroes/pages/home/HomePage', () => ({
     HomePage: () => <div data-testid="home-page"></div>
 }));
 
+vi.mock('@/heroes/pages/hero/HeroPage', () => ({
+    HeroPage: () => {
+        const { idSlug = '' } = useParams();
+        return (
+        <div data-testid="hero-page">HeroPage - {idSlug}</div>
+        )
+    },
+}));
+
 
 describe('appRouter', () => {
     test('should be configured as expected', () => {
@@ -20,10 +29,19 @@ describe('appRouter', () => {
     });
 
     test('should render home page at root path', () => {
-        render(<RouterProvider router={appRouter} />);
-
-        screen.debug();
+        const router = createMemoryRouter(appRouter.routes, {
+            initialEntries: ['/'],
+        });
+        render(<RouterProvider router={router} />);
         expect( screen.getByTestId('home-page') ).toBeDefined()
+    });
 
+    test('should render hero page at /heroes/:idSlug path', () => {
+        const router = createMemoryRouter(appRouter.routes, {
+            initialEntries: ['/heroes/superman'],
+        });
+        render(<RouterProvider router={router} />);
+        
+        expect( screen.getByTestId('hero-page').innerHTML).toContain('superman');
     });
 });
