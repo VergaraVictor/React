7f413856f7d2f92113139f00a9de52a319e63a59
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-06-25 08:47:58 -0500

    Parte2TestHomePage

--- React-2025/05-heroes-app/src/heroes/pages/home/HomePage.test.tsx
+++ React-2025/05-heroes-app/src/heroes/pages/home/HomePage.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { HomePage } from "./HomePage";
 import { MemoryRouter } from "react-router";
 import { usePaginatedHero } from "@/heroes/hooks/usePaginatedHero";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { FavoriteHeroProvider } from "@/heroes/context/FavoriteHeroContext";
 
 vi.mock('@/heroes/hooks/usePaginatedHero');
 
@@ -21,17 +22,54 @@ const queryClient = new QueryClient();
 const renderHomePage = (initialEntries:string[] = ['/']) => {
     return render(
         <MemoryRouter initialEntries={initialEntries}>
-            <QueryClientProvider client={queryClient}>
-                <HomePage />
-            </QueryClientProvider>
+            <FavoriteHeroProvider>
+                <QueryClientProvider client={queryClient}>
+                    <HomePage />
+                </QueryClientProvider>
+            </FavoriteHeroProvider>
         </MemoryRouter>
     );
 };
 
 
 describe('HomePage', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    // test('should render HomePage with default values', () => {
+    //     const { container } = renderHomePage();
+    //     expect(container).toMatchSnapshot();
+    // });
+
     test('should render HomePage with default values', () => {
-        const { container } = renderHomePage();
-        expect(container).toMatchSnapshot();
+        renderHomePage();
+        
+        const allTab = screen.getByRole('tab', { name: /all/i });
+        expect(allTab.getAttribute('aria-selected')).toBe('true'); // ✅ sin jest-dom
+        
+        expect(screen.getByRole('tab', { name: /favorites \(0\)/i })).toBeTruthy();
+        expect(screen.getByRole('tab', { name: /heroes/i })).toBeTruthy();
+        expect(screen.getByRole('tab', { name: /villains/i })).toBeTruthy();
+    });
+
+    test('should call usePaginatedHero with default values', () => {
+        renderHomePage();
+        expect(mockUsePaginatedHero).toHaveBeenCalledWith(1, 6, 'aa');
+    });
+
+    test('should call usePaginatedHero with custom query params', () => {
+        renderHomePage(['/?page=2&limit=10&category=villains']);
+        expect(mockUsePaginatedHero).toHaveBeenCalledWith(2, 10, 'villains');
+    });
+
+    test('should called usePanatedHero with default page and same limit on tab clicked', () => {
+        renderHomePage(['/?tab=favorites&page=2&limit=10']);
+
+        // const [allTabs, favoriteTab,  herosTab, villainsTab ] = screen.getAllByRole('tab');
+        const [, , , villainsTab ] = screen.getAllByRole('tab');
+        
+        fireEvent.click(villainsTab);
+        expect(mockUsePaginatedHero).toHaveBeenCalledWith(1, 10, 'villain');
     });
 });
