c91c241a2b687eb9fe9f493b80b9e0b9ed308638
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-08-11 08:27:38 -0500

    DiseñoProductsPage

--- React-2025/07-teslo-shop/teslo-shop/src/admin/components/AdminSidebar.tsx
+++ React-2025/07-teslo-shop/teslo-shop/src/admin/components/AdminSidebar.tsx
@@ -34,9 +34,13 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
     { icon: HelpCircle, label: 'Ayuda' },
   ];
 
+  console.log({ pathname });
+
   const isActiveRoute = ( to: string) => {
     // TODO: ajustarlo cuando estemos en la pantalla de producto.
-
+    if (pathname.includes('/admin/products/') && to ==='/admin/products' ){
+      return true;
+    } 
 
     return pathname === to; // true, false
 
--- React-2025/07-teslo-shop/teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
+++ React-2025/07-teslo-shop/teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
@@ -1,12 +1,66 @@
 import { AdminTitle } from "@/admin/components/AdminTitle"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
+import { Button } from "@base-ui/react"
+import { PlusIcon } from "lucide-react"
+import { Link } from "react-router"
 
 export const AdminProductsPage = () => {
     return (
         <>
-            <AdminTitle 
-                title="Productos"
-                subtitle="Aquí puedes ver y administrar tus productos"
-            />
+            <div className="flex justify-between items-center">
+                <AdminTitle 
+                    title="Productos"
+                    subtitle="Aquí puedes ver y administrar tus productos"
+                />
+
+                <Link to="/admin/products/new">
+                    <Button>
+                        <PlusIcon />
+                        Nuevo producto
+                    </Button>
+                </Link>
+            </div>
+
+            <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">ID</TableHead>
+                        <TableHead>Image</TableHead>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead>Precio</TableHead>
+                        <TableHead>Categoría</TableHead>
+                        <TableHead>Inventario</TableHead>
+                        <TableHead>Tallas</TableHead>
+                        <TableHead className="text-right">Acciones</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">1</TableCell>
+                        <TableCell>
+                            <img
+                                src='https://placehold.co/250x250'
+                                alt="Product"
+                                className="w-20 h20 object-cover rounder-md"
+                            />
+                        </TableCell>
+                        <TableCell>Product 1</TableCell>
+                        <TableCell>$250.00</TableCell>
+                        <TableCell>Categoría 1</TableCell>
+                        <TableCell>100 stock</TableCell>
+                        <TableCell>XS,S,L</TableCell>
+                        <TableCell className="text-right">
+                            {/* <Link to={`t-shirt-teslo`}> */}
+                            <Link to={`/admin/products/t-shirt-teslo`}>
+                                Editar
+                            </Link>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            
+            <CustomPagination totalPages={10} />
         </>
     )
 }
