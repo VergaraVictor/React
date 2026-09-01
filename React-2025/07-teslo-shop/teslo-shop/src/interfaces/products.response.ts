f1fc640ac77373b30210cd0683e42a0e291538bc
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-09-01 08:40:14 -0500

    AccionGetProductsActionVersin1

---
+++ React-2025/07-teslo-shop/teslo-shop/src/interfaces/products.response.ts
@@ -0,0 +1,320 @@
+{
+    "count": 52,
+    "pages": 6,
+    "products": [
+        {
+            "id": "0242a068-0d03-4eb7-9999-7850e69c41c3",
+            "title": "Men's Chill Full Zip Hoodie",
+            "price": 85,
+            "description": "Introducing the Tesla Chill Collection. The Men's Chill Full Zip Hoodie has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The hoodie features subtle thermoplastic polyurethane Tesla logos on the left chest and sleeve, a double layer single seam hood and pockets with custom matte zipper pulls. Made from 60% cotton and 40% recycled polyester.",
+            "slug": "men_chill_full_zip_hoodie",
+            "stock": 100,
+            "sizes": [
+                "XS",
+                "L",
+                "XL",
+                "XXL"
+            ],
+            "gender": "men",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1741111-00-A_0_2000.jpg",
+                "1741111-00-A_1.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "0741777a-408c-40a0-bb8e-f6f5cd2002dd",
+            "title": "Relaxed T Logo Hat",
+            "price": 30,
+            "description": "The Relaxed T Logo Hat is a classic silhouette combined with modern details, featuring a 3D T logo and a custom metal buckle closure. The ultrasoft design is flexible and abrasion resistant, while the inner sweatband includes quilted padding for extra comfort and moisture wicking. The visor is fully made from recycled plastic bottles. 100% Cotton.",
+            "slug": "relaxed_t_logo_hat",
+            "stock": 11,
+            "sizes": [
+                "XS",
+                "S",
+                "M",
+                "L",
+                "XL",
+                "XXL"
+            ],
+            "gender": "unisex",
+            "tags": [
+                "hats"
+            ],
+            "images": [
+                "1657932-00-A_1.jpg",
+                "1657932-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "14e49fad-fc50-4aff-a006-987d1110631c",
+            "title": "Kids Cyberquad Bomber Jacket",
+            "price": 65,
+            "description": "Wear your Kids Cyberquad Bomber Jacket during your adventures on Cyberquad for Kids. The bomber jacket features a graffiti-style illustration of our Cyberquad silhouette and wordmark. With three zippered pockets and our signature T logo and Tesla wordmark printed along the sleeves, Kids Cyberquad Bomber Jacket is perfect for wherever the trail takes you. Made from 60% cotton and 40% polyester.",
+            "slug": "kids_cyberquad_bomber_jacket",
+            "stock": 10,
+            "sizes": [
+                "XS",
+                "S",
+                "M"
+            ],
+            "gender": "kid",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1742702-00-A_1.jpg",
+                "1742702-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "1bcb7fa1-d0f0-4345-a89f-f9bf6a74f37e",
+            "title": "Kids Checkered Tee",
+            "price": 30,
+            "description": "The checkered tee is made from long grain, GMO free Peruvian cotton. Peru is the only country in the world where cotton is picked by hand on a large scale. The 4,500-year-old tradition prevents damage to the fiber during the picking process and removes the need to use chemicals to open the cotton plants before harvest. This environmentally friendly process results in cotton that is soft, strong, and lustrous – and the tee will get even softer with every wash.",
+            "slug": "kids_checkered_tee",
+            "stock": 10,
+            "sizes": [
+                "XS",
+                "S",
+                "M"
+            ],
+            "gender": "kid",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "100042307_alt_2000.jpg",
+                "100042307_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "20d09ba8-d8ca-4cb0-91b4-b6c8b325803c",
+            "title": "Women's Raven Joggers",
+            "price": 100,
+            "description": "Introducing the Tesla Raven Collection. The Women's Raven Joggers have a premium, relaxed silhouette made from a sustainable bamboo cotton blend. The joggers feature a subtle thermoplastic polyurethane Tesla wordmark and T logo and a french terry interior for a cozy look and feel in every season. Pair them with your Raven Slouchy Crew Sweatshirt, Raven Lightweight Zip Up Jacket or other favorite on the go fit. Made from 70% bamboo and 30% cotton.",
+            "slug": "women_raven_joggers",
+            "stock": 162,
+            "sizes": [
+                "XS",
+                "S",
+                "M",
+                "L",
+                "XL",
+                "XXL"
+            ],
+            "gender": "women",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1740270-00-A_1.jpg",
+                "1740270-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "2184c8a2-e6c2-4bcc-bba9-b3b37a692f34",
+            "title": "Women's Plaid Mode Tee",
+            "price": 35,
+            "description": "Designed to celebrate Tesla's incredible performance mode, the Plaid Mode Tee features great fit, comfort and style. Made from 100% cotton, it's the next best thing to riding shotgun at the Nürburgring.",
+            "slug": "women_plaid_mode_tee",
+            "stock": 16,
+            "sizes": [
+                "S",
+                "M"
+            ],
+            "gender": "women",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1549275-00-A_0_2000.jpg",
+                "1549275-00-A_1.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "21a79a10-7187-47ae-b0e2-01c1ee10d874",
+            "title": "Men's Quilted Shirt Jacket",
+            "price": 200,
+            "description": "The Men's Quilted Shirt Jacket features a uniquely fit, quilted design for warmth and mobility in cold weather seasons. With an overall street-smart aesthetic, the jacket features subtle silicone injected Tesla logos below the back collar and on the right sleeve, as well as custom matte metal zipper pulls. Made from 87% nylon and 13% polyurethane.",
+            "slug": "men_quilted_shirt_jacket",
+            "stock": 5,
+            "sizes": [
+                "XS",
+                "S",
+                "M",
+                "XL",
+                "XXL"
+            ],
+            "gender": "men",
+            "tags": [
+                "jacket"
+            ],
+            "images": [
+                "1740507-00-A_1.jpg",
+                "1740507-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "231bef83-f8ed-4d67-9d01-2428c8e2c489",
+            "title": "Men's Raven Lightweight Zip Up Bomber Jacket",
+            "price": 130,
+            "description": "Introducing the Tesla Raven Collection. The Men's Raven Lightweight Zip Up Bomber has a premium, modern silhouette made from a sustainable bamboo cotton blend for versatility in any season. The hoodie features subtle thermoplastic polyurethane Tesla logos on the left chest and below the back collar, a concealed chest pocket with custom matte zipper pulls and a french terry interior. Made from 70% bamboo and 30% cotton.",
+            "slug": "men_raven_lightweight_zip_up_bomber_jacket",
+            "stock": 10,
+            "sizes": [
+                "S",
+                "M",
+                "L",
+                "XL",
+                "XXL"
+            ],
+            "gender": "men",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1740250-00-A_1.jpg",
+                "1740250-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "272b21e0-3f3a-4f83-b0a8-2218296199d0",
+            "title": "Men's Turbine Long Sleeve Tee",
+            "price": 45,
+            "description": "Introducing the Tesla Turbine Collection. Designed for style, comfort and everyday lifestyle, the Men's Turbine Long Sleeve Tee features a subtle, water-based T logo on the left chest and our Tesla wordmark below the back collar. The lightweight material is double-dyed, creating a soft, casual style for ideal wear in any season. Made from 50% cotton and 50% polyester.",
+            "slug": "men_turbine_long_sleeve_tee",
+            "stock": 50,
+            "sizes": [
+                "XS",
+                "S",
+                "M",
+                "L"
+            ],
+            "gender": "men",
+            "tags": [
+                "shirt"
+            ],
+            "images": [
+                "1740280-00-A_0_2000.jpg",
+                "1740280-00-A_1.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        },
+        {
+            "id": "28084c73-34f7-411d-9ffb-623c08236129",
+            "title": "Chill Pullover Hoodie",
+            "price": 130,
+            "description": "Introducing the Tesla Chill Collection. The Chill Pullover Hoodie has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The unisex hoodie features subtle thermoplastic polyurethane Tesla logos across the chest and on the sleeve, a double layer single seam hood and pockets with custom matte zipper pulls. Made from 60% cotton and 40% recycled polyester.",
+            "slug": "chill_pullover_hoodie",
+            "stock": 10,
+            "sizes": [
+                "XS",
+                "S",
+                "M",
+                "L",
+                "XL",
+                "XXL"
+            ],
+            "gender": "unisex",
+            "tags": [
+                "hoodie"
+            ],
+            "images": [
+                "1740051-00-A_1.jpg",
+                "1740051-00-A_0_2000.jpg"
+            ],
+            "user": {
+                "id": "81e9ea44-49fa-4efe-8825-77d5020fd293",
+                "email": "test1@google.com",
+                "fullName": "Test One",
+                "isActive": true,
+                "roles": [
+                    "admin"
+                ]
+            }
+        }
+    ]
+}
