4929e1715832744b5cb6fdb6416321639e914c7e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-09-02 08:37:07 -0500

    AccionGetProductsAction

--- React-2025/07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
+++ React-2025/07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
@@ -1,11 +1,13 @@
 import { tesloApi } from "@/api/tesloApi"
+import type { ProductsResponse } from "@/interfaces/products.response";
 
 
 
 export const getProductsAction = async() => {
 
-    const {data} = await tesloApi.get('/products');
+    const {data} = await tesloApi.get<ProductsResponse>('/products');
 
+    console.log(data);
     return data;
 
 }
