670fc43514c5f8d62f8b9ad83fe3734677ddcae0
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-09-17 08:22:25 -0500

    MensajesEnPantallaSobreInicioDeSesión

--- React-2025/07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
+++ React-2025/07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
@@ -1,22 +1,38 @@
+import { Link, useNavigate } from "react-router";
+import { useState, type FormEvent } from 'react';
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 // import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label";
 import { Input } from "@base-ui/react";
 import { CustomLogo } from "@/components/custom/CustomLogo";
-import { Link } from "react-router";
-import type { FormEvent } from 'react';
+import { loginAction } from "@/auth/actions/login.action";
+import { toast } from "sonner";
 
 export const LoginPage = () => {
 
+  const navigate = useNavigate();
+  const [isPosting, setIsPosting] = useState(false)
+
   const handleLogin = async(event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsPosting(true); 
 
     const formData = new FormData(event.target as HTMLFormElement);
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
-    console.log({ email, password });    
+    try {
+      const data = await loginAction(email, password);    
+      localStorage.setItem('token', data.token);
+      console.log('re-redireccionando a al home');
+      navigate('/')      
+    } catch (error) {
+      toast.error('Correo o/y contraseña no validos')
+    }
+    
+    setIsPosting(false);
   };
 
   return (
@@ -42,7 +58,7 @@ export const LoginPage = () => {
                 </div>
                 <Input id="password" type="password" name="password" required placeholder="Contraseña"/>
               </div >
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Ingresar
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
