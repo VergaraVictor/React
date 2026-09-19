6f6309afab2d6c6aa7786e60c83f9268c0d36af6
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2026-09-19 08:02:38 -0500

    GestorDeEstadoZustand

--- React-2025/07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
+++ React-2025/07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@base-ui/react";
+import { useCounterStore } from "@/auth/store/auth.store";
+
 export const ProductPage = () => {
-    return <div>ProductPage</div>;
-}
+    
+    const { inc, count, dec, incBy } = useCounterStore();
+
+    return (
+        <>
+            <h1 className="text-3xl font-monserrat">Count: { count } </h1>
+
+            <Button onClick={inc}>+1</Button>
+            <Button onClick={dec}>-1</Button>
+            <Button onClick={() => incBy(5)}>+5</Button>
+        </>
+    );
+};
